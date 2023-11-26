44d0b591730ee108887892da60ffe533bd3c8a4c
Ginga Alexandra <alexandra.ginga@stud.ubbcluj.ro>
2023-11-26 23:53:51 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ginga_Alexandra_Lab7;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ginga_Alexandra_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
